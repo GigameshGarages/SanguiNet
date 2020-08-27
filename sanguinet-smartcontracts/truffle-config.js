5df7a02a9f99adb7e2f75e6375d843d99d18273b
Gokul Alex <gokulsan@live.com>
2020-08-27 19:43:30 +0530

    modified:   sanguinet-smartcontracts/truffle-config.js

--- sanguinet-smartcontracts/truffle-config.js
+++ sanguinet-smartcontracts/truffle-config.js
@@ -89,7 +89,7 @@ module.exports = {
 
   // Set default mocha options here, use special reporters etc.
   mocha: {
-    // timeout: 100000
+     timeout: 120000
   },
 
   // Configure your compilers
